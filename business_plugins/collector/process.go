de696a72ce6e77a18aad224719f0f22aaf96a51a
haolipeng <kathleenchadzs10@gmail.com>
2026-01-26 16:26:47 +0800

    进程资产的字段映射问题

--- business_plugins/collector/process.go
+++ business_plugins/collector/process.go
@@ -51,7 +51,7 @@ func (h *ProcessHandler) Handle(c *businessplugins.Client, cache *engine.Cache, 
 			rec.Data.Fields["cwd"], _ = p.Cwd()
 			rec.Data.Fields["checksum"], _ = p.ExeChecksum()
 			rec.Data.Fields["exe_hash"], _ = p.ExeHash()
-			rec.Data.Fields["exe"], _ = p.Exe()
+			rec.Data.Fields["path"], _ = p.Exe()
 			rec.Data.Fields["pid"] = p.Pid()
 			mapstructure.Decode(stat, &rec.Data.Fields)
 			mapstructure.Decode(status, &rec.Data.Fields)
@@ -61,7 +61,7 @@ func (h *ProcessHandler) Handle(c *businessplugins.Client, cache *engine.Cache, 
 			rec.Data.Fields["container_name"] = m["container_name"]
 			rec.Data.Fields["integrity"] = "true"
 			// only for host files
-			if _, ok := cache.Get(5057, rec.Data.Fields["exe"]); ok && rec.Data.Fields["container_id"] == "" {
+			if _, ok := cache.Get(5057, rec.Data.Fields["path"]); ok && rec.Data.Fields["container_id"] == "" {
 				rec.Data.Fields["integrity"] = "false"
 			}
 			rec.Data.Fields["package_seq"] = seq
